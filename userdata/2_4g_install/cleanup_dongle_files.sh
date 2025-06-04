a3f6f65540be453a643f99914f441b634233d8dc
Rion <17915121+Redemp@users.noreply.github.com>
2025-06-04 16:14:13 +0200

    Update cleanup_dongle_files.sh
    Fixed dev path

--- userdata/2_4g_install/cleanup_dongle_files.sh
+++ userdata/2_4g_install/cleanup_dongle_files.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color
 echo -e "${YELLOW}Deleting dongles.conf if it exists...${NC}"
 rm -f /userdata/system/configs/dongles.conf && echo -e "${GREEN}✔ Removed dongles.conf${NC}"
 
-echo -e "${YELLOW}Deleting all *.json files in /userdata/system/usb_test/dongles/...${NC}"
+echo -e "${YELLOW}Deleting all *.json files in /userdata/system/dongle_2_4g/dongles/...${NC}"
 find /userdata/system/usb_test/dongles/ -type f -name '*.json' -exec rm -v {} \;
 
 echo -e "${YELLOW}Deleting all 30-wake-on*.rules files in /etc/udev/rules.d/...${NC}"
